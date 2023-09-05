9a40375bda25f317f7dcb4f04f2ad18e832e6c81
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-09-05 15:52:15 -0500

    server setup

--- server.js
+++ server.js
@@ -5,4 +5,9 @@ const expressLayouts = require('express-ejs-layouts')
 
 app.set('view engine', 'ejs')
 
-app.set
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.listen(process.env.PORT ||3000)
