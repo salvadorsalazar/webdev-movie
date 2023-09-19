c30fd7958f9900ecf8a9c0a632dedd5ae19adc78
salvadorsalazar <salvador.g.salazar79@gmail.com>
2023-09-19 16:16:19 -0500

    books routes and models

---
+++ routes/books.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+
+// all books routes
+router.get('/', async (req, res) => {
+
+})
+
+// new book routes
+router.get('/new', (req, res) => {
+  
+})
+
+// create book route
+router.post('/', async (req, res) => {
+ 
+})
+
+module.exports = router
